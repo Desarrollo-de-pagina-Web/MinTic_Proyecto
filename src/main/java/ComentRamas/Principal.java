d0746ff0c70011abe90dc34b2b04801a4b78b7af
Santiago <jefferson02830@gmail.com>
2022-08-26 19:09:31 -0500

    Creacion de ramas principales

--- src/main/java/ComentRamas/Principal.java
+++ src/main/java/ComentRamas/Principal.java
@@ -3,6 +3,7 @@ package ComentRamas;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("comentario de Jefferson Santiago Pinchao");
+        System.out.println("creacion de ramas principales development y release");
 
     }
 
