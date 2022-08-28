0ad365e0c4b0af8f3ccc1d9a5874fdce60cf4a94
AmparoClavijo <amparo.0014@hotmail.com>
2022-08-27 21:12:07 -0500

    Primer commit Amparo Clavijo

--- src/main/java/ComentRamas/Principal.java
+++ src/main/java/ComentRamas/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("comentario de Jefferson Santiago Pinchao");
 
+        System.out.println("Primer Commit Amparo Clavijo");
+
     }
 
 }
