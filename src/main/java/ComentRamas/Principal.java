035d836b2359e61910f83e3ba0b627ba473a1fbf
AmparoClavijo <amparo.0014@hotmail.com>
2022-08-27 21:12:07 -0500

    Jhonatan Chavarro

--- src/main/java/ComentRamas/Principal.java
+++ src/main/java/ComentRamas/Principal.java
@@ -4,6 +4,10 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("comentario de Jefferson Santiago Pinchao");
 
+        System.out.println("Primer Commit Amparo Clavijo");
+
+        System.out.println("Primer comentario de Jhonatan Chavarro Arias");
+
     }
 
 }
