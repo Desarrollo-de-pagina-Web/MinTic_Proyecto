5b586d8fc90f8b64688c7a90882076b5cbbb601b
@german358 <german.358@hotmail.com>
2022-08-31 21:04:46 -0500

    commit de empleado de german castro

--- src/main/java/Sprint2/Empleado.java
+++ src/main/java/Sprint2/Empleado.java
@@ -1,9 +1,47 @@
 package Sprint2;
 
+enum tipo_Rol{
+    administrativo,operativo
+}
+
 
 
 public class Empleado {
+private  String nombreEmpleado;
+private  String correoEmpleado;
+private  tipo_Rol tipo;
+private Empresa  emp;
 
 
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
 
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public tipo_Rol getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(tipo_Rol tipo) {
+        this.tipo = tipo;
+    }
+
+    public Empresa getEmp() {
+        return emp;
+    }
+
+    public void setEmp(Empresa emp) {
+        this.emp = emp;
+    }
 }
