b3c75867a540c70c2c9414f06a54e2f745b5ee07
JhonatanChavarroArias <jchavarro4@misena.edu.co>
2022-09-05 09:30:38 -0500

    Codigo clase Movimiento de dinero (jhonatan chavarro)

--- src/main/java/Sprint2/MovimientoDinero.java
+++ src/main/java/Sprint2/MovimientoDinero.java
@@ -1,5 +1,27 @@
 package Sprint2;
+import java.util.Date;
 
 public class MovimientoDinero {
-    //código
+    public class Transaccion {
+        private long Id;
+        private String Concepto;
+        private float Cantidad;
+        //private Date createdAt;
+        //private Date updatedAt;
+
+        public long getId() {
+            return Id;
+        }
+
+        public String getConcepto() {
+            return Concepto;
+        }
+
+        public float getCantidad() {
+            return Cantidad;
+        }
+
+    }
+
+
 }
